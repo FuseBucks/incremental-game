4947bd9c8c53ea4abbe55ce77073160bc4e291b8
John Julius David <johnjuliusdavid.business@gmail.com>
2025-09-16 22:50:05 +0800

    feat: Add data and virus buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
-import { Test } from "./components/Test";
+"use client";
+import { useState } from "react";
+import VirusButton from "./components/main_window/virus_button";
+import DataButton from "./components/main_window/data_button";
 
 export default function Home() {
-  return <Test />;
+  const [dataCount, setDataCount] = useState(0);
+  const [virusCount, setVirusCount] = useState(0);
+
+  const handleDataClick = () => {
+    setDataCount(dataCount + 1);
+  };
+
+  const handleVirusClick = () => {
+    setVirusCount(virusCount + 1);
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-600">
+      <div className="bg-white p-8 rounded-xl shadow-lg border border-gray-200">
+        <div className="flex gap-4">
+          <DataButton onClick={handleDataClick} />
+          <VirusButton onClick={handleVirusClick} />
+        </div>
+      </div>
+    </div>
+  );
 }
