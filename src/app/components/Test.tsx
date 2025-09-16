5d145f63b643fc271aca4a18919ea954a634a1bf
ega <202210027@fit.edu.ph>
2025-09-16 22:24:46 +0800

    chore: fresh start

---
+++ src/app/components/Test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Test() {
+  return (
+    <p className="rounded-lg bg-white p-4 font-bold text-red-500 hover:bg-gray-400">
+      TEST!
+    </p>
+  );
+}
