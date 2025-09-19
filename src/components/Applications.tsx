f2ec6f6d4de9e4af99919098fd75878b9537d404
Ervzs <ejpejo26@gmail.com>
2025-09-19 17:55:13 +0800

    feat: Files are now interactable

--- src/components/Applications.tsx
+++ src/components/Applications.tsx
@@ -14,7 +14,7 @@ export function Applications({
 
   return (
     <div
-      className="flex h-22 w-24 cursor-pointer flex-col items-center justify-center px-2 text-center select-none"
+      className="flex h-22 w-24 cursor-pointer flex-col items-center justify-center px-1 text-center select-none"
       onClick={handleOpenWindow}
     >
       <img className="h-12 w-12" src="folder.png" alt="App icon" />
