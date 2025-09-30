d07ea1c1150ec900c60a5e5fea785fde6b54270d
Ervzs <ejpejo26@gmail.com>
2025-09-30 15:57:42 +0800

    Imported analytics vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Analytics } from "@vercel/analytics/next";
 import { MainWindow } from "../components/MainWindow";
 import { Applications } from "../components/Applications";
 import { TaskBar } from "../components/TaskBar";
